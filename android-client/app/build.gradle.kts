1f86bca087189606d26baa4b3641c81e8b49dd33
Antonio José <antonioj2004@gmail.com>
2026-05-12 13:03:14 +0200

    feat: real android training

--- android-client/app/build.gradle.kts
+++ android-client/app/build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.compose)
+    id("com.chaquo.python")
 }
 
 android {
@@ -19,6 +20,10 @@ android {
         versionName = "1.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+
+        ndk {
+            abiFilters += listOf("x86_64", "arm64-v8a")
+        }
     }
 
     buildTypes {
@@ -58,4 +63,17 @@ dependencies {
 
     // Libreria para hacer llamadas HTTP (API REST)
     implementation("com.squareup.okhttp3:okhttp:4.12.0")
+}
+
+// Configuracion de Chaquopy (Python en Android)
+chaquopy {
+    defaultConfig {
+        version = "3.13"
+        pip {
+            buildPython("C:/Users/user/venv/Scripts/python.exe")
+            // Instalamos lo mismo que usas en el PC
+            install("numpy")
+            install("tensorflow")
+        }
+    }
 }
